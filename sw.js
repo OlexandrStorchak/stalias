514538498d506fa452cc98cbeba645b7ee8cf3ec
Storchak Olexandr <alexsstorchak@gmail.com>
2024-07-13 18:20:22 +0300

    Add words by AI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'st-alias-pwa-v8'
+const CACHE_NAME = 'st-alias-pwa-v9'
 const urlsToCache = ['index.html', 'game.html', 'styles.css', 'scripts.js', 'manifest.json', 'favicon.png', 'logo.png']
 
 self.addEventListener('install', (event) => {
